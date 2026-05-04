7d847109e860fb1c76b9027910af5fd9f776b3ad
Atikul Islam Sajib <atikulislamsajib137@gmail.com>
2026-05-04 16:19:17 +0200

    POST is updated

--- GET/tutorial2.py
+++ GET/tutorial2.py
@@ -10,8 +10,10 @@ def load_ecommerce_database():
     with open("ecommerce.json", "r") as file:
         return json.load(file)
 
+class Orders(BaseModel):
+    status: Optional[Annotated[str, Field(description="Status of the order", examples=["pending", "completed", "cancelled"])]] = None
 
-@app.get("/user/{user_id}/orders}")
+@app.get("/user/{user_id}/{orders}")
 def display_all_delivered_orders(user_id: int, status: str):
 
     products_detail = load_ecommerce_database()
@@ -65,3 +67,24 @@ def filter_dataset(role: str, active: bool, city: str):
         "total": len(detailed_information),
         "users": detailed_information,
     }
+
+@app.put("/users/{user_id}/orders/{order_id}")
+def update_details(user_id: int, order_id: int, status: Orders):
+    orders_detail = load_ecommerce_database()
+    
+    specific_information = []
+    orders_information = []
+    for details in orders_detail:
+        if details["id"] == user_id:
+            specific_information.append(details)
+
+    for details in specific_information:
+        for order in details["orders"]:
+            if order["order_id"] == order_id:
+                order["status"] = status.status
+                orders_information.append(order)
+                with open("ecommerce.json", "w") as file:
+                    json.dump(orders_detail, file, indent=4)
+        
+                
+    return orders_information
