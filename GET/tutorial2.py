60a65b26b77150359fd0a16fdc65f9867b3a3fad
Atikul Islam Sajib <atikulislamsajib137@gmail.com>
2026-05-04 14:28:03 +0200

    GET is updated with another practise

---
+++ GET/tutorial2.py
@@ -0,0 +1,40 @@
+import json
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import Optional, Annotated, List, Dict
+
+app = FastAPI()
+
+
+def load_ecommerce_database():
+    with open("ecommerce.json", "r") as file:
+        return json.load(file)
+
+
+@app.get("/user/{user_id}/orders}")
+def display_all_delivered_orders(user_id: int , status: str):
+    
+    products_detail = load_ecommerce_database()
+
+    specific_details = []
+
+    for details in products_detail:
+        while user_id == details["id"]:
+            specific_details.append(details)
+            break
+
+    ordered_information = []
+
+    for details in specific_details:
+        delivered_information = details["orders"]
+        for order in delivered_information:
+            if order["status"] == status:
+                ordered_information.append(order)
+
+    result = {
+        "user_id": specific_details[0]["id"],
+        "name": specific_details[0]["name"],
+        "orders": ordered_information,
+        "total_results": len(ordered_information),
+    }
+    return result
